b926e19a697039633ea8efb7ea5f13dcaba48029
Dustin Rhodes <dustin.rhodes@lacity.org>
2015-06-15 16:18:15 -0700

    LC-833 hides extra height when drop-down menu visible

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,22 @@
 $(document).ready(function(){
 
+	var width = $(window).width();
+	var height = $(window).height();
+	
+	// Drop-down menu 
+
 	$(".menu").click (function(){
-		$(".drop-down").slideToggle("slow");
+		if ($(".drop-down").is(":visible")) {
+			$("body").css({"width": "initial", "height": "initial", "overflow": "visible"});
+			$(".drop-down").css({"width": width, "height": height, "overflow": "hidden"});
+			$(".drop-down").slideUp("slow");
+		} else {
+			$("body").css({"width": width, "height": height, "overflow": "hidden"});
+			$(".drop-down").css({"width": width + 17, "height": height, "overflow": "hidden"});
+			$(".drop-down").slideDown("slow");
+
+		};
+
 	})
 
 });
