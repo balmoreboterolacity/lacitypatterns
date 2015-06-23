5949f040cdb9da6e28af7cca13f0ce2df0453da4
unknown <ITAcid393@ITA-BBP3JQ1.cds.ci.la.ca.us>
2015-06-23 16:06:01 -0700

    Slider updates

--- source/js/script.js
+++ source/js/script.js
@@ -33,5 +33,19 @@ $(document).ready(function(){
 	$('.nav-button.search').click (function() {
 		$('.search-form').slideToggle("slow");
 	});
+	
+	$('#slider .fa-camera').click(function() {
+      if (!$('.field-caption-content').hasClass('clicked')) {
+        $('.field-caption-content').addClass('clicked').css({
+          'display': 'block'
+        });
+        $('#slider .fa-camera').addClass('orange');
+      } else {
+        $('.field-caption-content').removeClass('clicked').css({
+          'display': 'none'
+        });
+        $('#slider .fa-camera').removeClass('orange');
+      }
+    });
 
 });
